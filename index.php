139c4bd8a7cfa3206052c69b60c6166944221e17
Jose Viscasillas <viscasillas@me.com>
2014-03-29 18:22:26 -0400

    New terminal interface

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
 <?
 // Start Application
-//
 require_once('System/Core/Start.php');
-// Core::AppOverride('json','
-// {
-// 	"Mode":"Run",
-// 	"Title":"PayMyPhoneBill",
-// 	"Author":"Jose Viscasillas",
-// 	"Description":"Pay Phonebills over the internet",
-// 	"Library":{
-// 		"Package":[
-// 			"jQuery\/jquery-2.1.0.min.js",
-// 			"Bootstrap\/css\/bootstrap.css",
-// 			"Bootstrap\/js\/bootstrap.js",
-// 			"Angular\/angular.min.js"
-// 		]
-// 	},
-// 	"Routes":{
-// 		"Route":[
-// 			{
-// 				"Input":"Home",
-// 				"Output":"?view=Home"
-// 			},{
-// 				"Input":"Photo",
-// 				"Output":"?view=Photo"
-// 			},{
-// 				"Input":"Form",
-// 				"Output":"?view=Form"
-// 			}
-// 		]
-// 	}
-// }
-// ');
 ?>
